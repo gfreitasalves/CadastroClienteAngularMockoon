082b4be5d9c65bfe024860905dcf6502cab51de3
gfreitasalves <gfreitasalves@gmail.com>
2024-02-22 13:41:40 -0300

    Adição Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  providers:[],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
