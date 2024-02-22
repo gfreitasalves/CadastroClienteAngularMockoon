4ba7168d6f8cd07ecdedea47fb553a07e72a3067
gfreitasalves <gfreitasalves@gmail.com>
2024-02-22 13:57:18 -0300

    Adionando módulos básicos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListagemClientesComponent } from './cliente/listagem-clientes/listagem-clientes.component';
 
 export const routes: Routes = [
-    
+    { path: '', component: HomeComponent },
+    { path: 'clientes', component: ListagemClientesComponent }
 ];
